419327b43c5d8e0d2da65fb807a6d2cdd1ef057d
CameraTim <93443049+CameraTim@users.noreply.github.com>
2022-03-26 15:48:50 -0400

    Update Polymorphism.py

--- Class_Inheritance_Assignment/Polymorphism.py
+++ Class_Inheritance_Assignment/Polymorphism.py
@@ -5,22 +5,25 @@ class Computer:
     monitors = 3
     RAM = "32GB"
     
+    #defining parent class function
     def Components(parts):
         CPU = "3700X"
         winVer = 10
         print("Your computer is a {}, with a {}, {}, and {}.".format(parts.comType,parts.RAM,CPU,winVer))
         
-#Adding 
+#Adding child class
 class Mac(Computer):
     comType = "Laptop"
     touchbar = False
     ports = 4
 
+    #defining child class function
     def Components(parts):
         CPU = "M1"
         OS = "Monterey"
         print("Your computer is a {}, with a {}, {}, and {}.".format(parts.comType,parts.RAM,CPU,OS))
 
+#Invokes methods for the parent and child classes
 PC = Computer()
 PC.Components()
 Apple = Mac()
