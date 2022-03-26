180dc14961e7c0801c003bdbcfa9728cef2136da
CameraTim <93443049+CameraTim@users.noreply.github.com>
2022-03-26 18:47:16 -0400

    Added Polymorphism Assignment

---
+++ Polymorphism_Assignment/Polymorphism.py
@@ -0,0 +1,28 @@
+#Creating the parent class
+class Computer:
+    comType = "Desktop"
+    touchScreen = True
+    monitors = 3
+    RAM = "32GB"
+    #defining parent class function
+    def Components(parts):
+        CPU = "3700X"
+        winVer = 10
+        print("Your computer is a {}, with a {}, {}, and {}.".format(parts.comType,parts.RAM,CPU,winVer))
+        
+#Adding child class
+class Mac(Computer):
+    comType = "Laptop"
+    touchbar = False
+    ports = 4
+    #defining child class function
+    def Components(parts):
+        CPU = "M1"
+        OS = "Monterey"
+        print("Your computer is a {}, with a {}, {}, and {}.".format(parts.comType,parts.RAM,CPU,OS))
+
+#Invokes methods for the parent and child classes
+PC = Computer()
+PC.Components()
+Apple = Mac()
+Apple.Components()
