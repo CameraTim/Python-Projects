0ef846c586597f9f8dfed8b80d4534c6e7682a5a
CameraTim <93443049+CameraTim@users.noreply.github.com>
2022-03-28 23:10:44 -0400

    Fixed second child class

--- Polymorphism_Assignment/Polymorphism.py
+++ Polymorphism_Assignment/Polymorphism.py
@@ -10,19 +10,32 @@ class Computer:
         winVer = 10
         print("Your computer is a {}, with a {}, {}, and {}.".format(parts.comType,parts.RAM,CPU,winVer))
         
-#Adding child class
+#Adding first child class
 class Mac(Computer):
     comType = "Laptop"
     touchbar = False
     ports = 4
-    #defining child class function
+    #defining first child class function
     def Components(parts):
         CPU = "M1"
         OS = "Monterey"
         print("Your computer is a {}, with a {}, {}, and {}.".format(parts.comType,parts.RAM,CPU,OS))
 
+#Adding second child class
+class PC(Computer):
+    touchScreen = False
+    ports = 8
+    RAM = "16GB"
+    #defining second child class function
+    def Components(parts):
+        CPU = "5950X"
+        winVer = 11
+        print("Your computer is a {}, with a {}, {}, and {}.".format(parts.comType,parts.RAM,CPU,winVer))
+
 #Invokes methods for the parent and child classes
-PC = Computer()
-PC.Components()
+firstBuild = Computer()
+firstBuild.Components()
 Apple = Mac()
 Apple.Components()
+Windows = PC()
+Windows.Components()
